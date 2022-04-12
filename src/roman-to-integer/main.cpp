0dea38af6ea6f7529c9f40dd4a009a6db26288cc
Egor Morunov <e_morunov@wargaming.net>
2022-04-12 17:01:59 +0300

    solve roman-to-integer

---
+++ src/roman-to-integer/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "solution.h"
+
+int main() {
+    std::cout << Solution::romanToInt("MCMXCIV") << std::endl;
+    return 0;
+}
---
+++ src/roman-to-integer/solution.h
@@ -0,0 +1,34 @@
+#include <string>
+
+class Solution {
+public:
+
+    static int romanToInt(std::string s) {
+        int result{0}, value{0}, rightSymbolValue{0};
+        if (!s.empty()) {
+            rightSymbolValue = romanFigureToInt(s.back());
+            result += rightSymbolValue;
+        }
+        for (int i = s.size() - 2; i >= 0; --i) {
+            value = romanFigureToInt(s[i]);
+            result += value < rightSymbolValue ? -value : value;
+            rightSymbolValue = value;
+        }
+        return result;
+    }
+
+private:
+
+    static int romanFigureToInt(char symbol) {
+        switch (symbol) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
+};
