3cc934e9e6c1404c57c4f0012f53bfb3e5b9c065
Egor Morunov <e_morunov@wargaming.net>
2025-06-23 15:22:58 +0200

    solve 3sum

--- src/3sum/solution.h
+++ src/3sum/solution.h
@@ -1,63 +1,48 @@
+#include <algorithm>
 #include <cstdint>
-#include <iostream>
-#include <unordered_map>
 #include <vector>
 
 class Solution {
 public:
+    // O(n^2)
     static std::vector<std::vector<int32_t>> threeSum(std::vector<int32_t>& nums) {
-        std::unordered_map<int32_t, std::vector<size_t>> values;
-        for (size_t i = 0; i < nums.size(); ++i) {
-            auto it = values.find(nums[i]);
-            if (it == values.end()) {
-                values.emplace(nums[i], std::vector<size_t>{i});
-            } else {
-                it->second.push_back(i);
-            }
+        if (nums.size() < 3) {
+            return {};
         }
+        std::sort(nums.begin(), nums.end());
         std::vector<std::vector<int32_t>> result;
-        // tree.print();
-        for (const auto& pair1 : values) {
-            for (const auto& pair2 : values) {
-                int32_t v1 = pair1.first;
-                int32_t v2 = pair2.first;
-                int32_t v3 = - v1 - v2;
-                // we should skip if there's only one element with such a value
-                if (v1 == v2 && pair1.second.size() < 2) {
-                    continue;
+        const size_t s = nums.size();
+        size_t i1 = 0;
+        do {
+            const int32_t v1 = nums[i1];
+            size_t i2 = i1 + 1;
+            size_t i3 = s - 1;
+            do {
+                const int32_t v2 = nums[i2];
+                const int32_t v3 = nums[i3];
+                const int32_t sum = v1 + v2 + v3;
+                if (sum == 0) {
+                    result.push_back({v1, v2, v3});
+                    do {
+                        ++i2;
+                    } while (nums[i2] == v2 && i2 < i3);
+                    do {
+                        --i3;
+                    } while (nums[i3] == v3 && i2 < i3);
+                } else if (sum < 0) {
+                    do {
+                        ++i2;
+                    } while (nums[i2] == v2 && i2 < i3);
+                } else {
+                    do {
+                        --i3;
+                    } while (nums[i3] == v3 && i2 < i3);
                 }
-                const auto it = values.find(v3);
-                // we should skip if no element with such value
-                if (it == values.end()) {
-                    continue;
-                }
-                // we should skip if there's only one element with such a value
-                if ((v3 == v1 || v3 == v2) && it->second.size() < (v1 == v2 ? 3 : 2)) {
-                    continue;
-                }
-                if (v2 < v1) {
-                    std::swap(v1, v2);
-                }
-                if (v3 < v1) {
-                    std::swap(v1, v3);
-                }
-                if (v3 < v2) {
-                    std::swap(v2, v3);
-                }
-                bool is_unique = true;
-                for (const auto& r : result) {
-                    if (v1 == r[0] && v2 == r[1] && v3 == r[2]) {
-                        is_unique = false;
-                        break;
-                    }
-                }
-                // we should skip if tuple is not unique
-                if (!is_unique) {
-                    continue;
-                }
-                result.push_back({v1, v2, v3});
-            }
-        }
+            } while (i2 < i3);
+            do {
+                ++i1;
+            } while (nums[i1] == v1 && i1 < s - 2);
+        } while (i1 < s - 2);
         return result;
     }
 };
